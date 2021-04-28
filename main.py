7e4a654c3b7b678c9daf19a5bf421578d07047d7
Magistr <magistrdev@gmail.com>
2021-04-29 01:36:25 +0300

    fix main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import initant as ant
 import plane
-
+import time
+import math
 ant.def_pos()
 
 def pos_arm(index, x,y,z):
@@ -9,7 +10,19 @@ def pos_arm(index, x,y,z):
     pl = plane.planes[index]
     ant.set_arm_ang(index,pl.links[0].angle, pl.links[1].angle, pl.links[2].angle)
 
-pos_arm(0, 100, -50, 100)
-pos_arm(2, 100, -50, 100)
-pos_arm(3, 100, -50, 100)
-pos_arm(5, 100, -50, 100)
+
+
+# pos_arm(0, 100, -50, 100)
+# pos_arm(2, 100, -50, 100)
+# pos_arm(3, 100, -50, 100)
+# pos_arm(5, 100, -50, 100)
+
+angle = 0
+x1 = 100
+z1 = 100
+
+while 1:
+    x = x1 + (r * math.cos(angle))
+    y = z1 + (r + math.sin(angle))
+    pos_arm(0, x, 0, y)
+    angle = angle + 0.1
