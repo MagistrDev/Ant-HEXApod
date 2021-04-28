2fd0a9b866c9e2be04b759ec39564ec37806941a
Magistr <magistrdev@gmail.com>
2021-04-28 21:21:12 +0300

    test

---
+++ drv/pca9685drv.c
@@ -0,0 +1,34 @@
+#include "pca9685drv.h"
+
+
+int main(void) {
+  int fd = open("/dev/i2c-1", O_RDWR | O_NONBLOCK);
+  if (fd == -1) {
+    printf("Failed to open /dev/i2c-1\n");
+    return 1;
+  }
+
+  int status = ioctl(fd, I2C_SLAVE, 0x60);
+  if (status == -1) {
+    printf("Failed to acquire bus access and/or talk to slave.\n");
+    return 2;
+  }
+
+  unsigned char buffer[1] = {0};
+  buffer[0] = 0x0C;
+  status = write(fd, buffer, 1);
+  if (status == -1) {
+    printf("Failed to write register 0x0C.\n");
+    return 3;
+  }
+
+  status = read(fd, buffer, 1);
+  if (status == -1) {
+    printf("Failed to read from register 0x0C.\n");
+    return 4;
+  }
+
+  printf("WHO_AM_I = 0x%X\n", (int)buffer[0]);
+
+  close(fd);
+}
---
+++ drv/pca9685drv.h
@@ -0,0 +1,12 @@
+#ifndef _PCA9685_DRV_H_
+#define _PCA9685_DRV_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+
+
+#endif
