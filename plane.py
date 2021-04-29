3e2eaf9ff079f18bad1f50259b929c2714e1ee93
Magistr <magistrdev@gmail.com>
2021-04-29 06:17:08 +0300

    fix py

--- plane.py
+++ plane.py
@@ -4,28 +4,29 @@ LINK_COXA = 0
 LINK_FEMUR = 1
 LINK_TIBIA = 2
 
-class LinkInfo(object):
-    def __init__(self):
+class LinkInfo():
+    def __init__(self, length = 0, zero_rotate = 0, min_angle = 0, max_angle = 0, angle = 0):
         #  Current link state
-        angle = 0.0
+        self._angle = 0.0
         #  Link configuration
-        length = 0
-        zero_rotate = 0
-        min_angle = 0
-        max_angle = 0
+        self._length = 0
+        self._zero_rotate = 0
+        self._min_angle = 0
+        self._max_angle = 0
 
-class point_3d_t(object):
-    def __init__(self):
-        x = 0.0
-        y = 0.0
-        z = 0.0
+class point_3d_t:
+    def __init__(self,x = 0,y = 0,z = 0):
+        self._x = x
+        self._y = y
+        self._z = z
 
-class LimbInfo(object):
+class LimbInfo():
     def __init__(self):
-        position = point_3d_t()
+        self._position = point_3d_t()
+        self._links = [LinkInfo(),LinkInfo(),LinkInfo()]
+
         # path_3d_t  movement_path
         # link_info_t links[3]
-        links = [LinkInfo(),LinkInfo(),LinkInfo()]
 
 planes = [LimbInfo(),LimbInfo(),LimbInfo(),LimbInfo(),LimbInfo(),LimbInfo()]
 
@@ -38,9 +39,9 @@ def DEG_TO_RAD(deg):
 def set_point(index,x,y,z):
     global planes
     obj = planes[index]
-    obj.position.x = x
-    obj.position.y = y
-    obj.position.z = z
+    obj._position._x = x
+    obj._position._z = y
+    obj._position._z = z
 
 # ***************************************************************************
 # @brief  Calculate angles
@@ -50,15 +51,15 @@ def set_point(index,x,y,z):
 def kinematic_calculate_angles(pindex):
     global planes
     info = planes[pindex]
-    coxa_zero_rotate_deg = info.links[LINK_COXA].zero_rotate
-    femur_zero_rotate_deg = info.links[LINK_FEMUR].zero_rotate
-    tibia_zero_rotate_deg = info.links[LINK_TIBIA].zero_rotate
-    coxa_length = info.links[LINK_COXA].length
-    femur_length = info.links[LINK_FEMUR].length
-    tibia_length = info.links[LINK_TIBIA].length
-    x = info.position.x
-    y = info.position.y
-    z = info.position.z
+    coxa_zero_rotate_deg = info.links[LINK_COXA]._zero_rotate
+    femur_zero_rotate_deg = info.links[LINK_FEMUR]._zero_rotate
+    tibia_zero_rotate_deg = info.links[LINK_TIBIA]._zero_rotate
+    coxa_length = info.links[LINK_COXA]._length
+    femur_length = info.links[LINK_FEMUR]._length
+    tibia_length = info.links[LINK_TIBIA]._length
+    x = info._position._x
+    y = info._position._z
+    z = info._position._z
     # Move to (X*, Y*, Z*) coordinate system - rotate
     coxa_zero_rotate_rad = DEG_TO_RAD(coxa_zero_rotate_deg)
     x1 = x * math.cos(coxa_zero_rotate_rad) + z * math.sin(coxa_zero_rotate_rad)
@@ -66,7 +67,7 @@ def kinematic_calculate_angles(pindex):
     z1 = -x * math.sin(coxa_zero_rotate_rad) + z * math.cos(coxa_zero_rotate_rad)
     # Calculate COXA angle
     coxa_angle_rad = math.atan2(z1, x1)
-    info.links[LINK_COXA].angle = RAD_TO_DEG(coxa_angle_rad)
+    info.links[LINK_COXA]._angle = RAD_TO_DEG(coxa_angle_rad)
     # Prepare for calculation FEMUR and TIBIA angles
     # Move to (X*, Y*) coordinate system (rotate on axis Y)
     x1 = x1 * math.cos(coxa_angle_rad) + z1 * math.sin(coxa_angle_rad)
@@ -85,15 +86,15 @@ def kinematic_calculate_angles(pindex):
     alpha = math.acos( (b * b + c * c - a * a) / (2 * b * c))
     gamma = math.acos( (a * a + b * b - c * c) / (2 * a * b))
     # Calculate FEMUR and TIBIA angle
-    info.links[LINK_FEMUR].angle = femur_zero_rotate_deg - RAD_TO_DEG(alpha) - RAD_TO_DEG(fi)
-    info.links[LINK_TIBIA].angle = RAD_TO_DEG(gamma) - tibia_zero_rotate_deg
+    info.links[LINK_FEMUR]._angle = femur_zero_rotate_deg - RAD_TO_DEG(alpha) - RAD_TO_DEG(fi)
+    info.links[LINK_TIBIA]._angle = RAD_TO_DEG(gamma) - tibia_zero_rotate_deg
     # print(info.links[LINK_COXA].angle)
     # print(info.links[LINK_FEMUR].angle)
     # print(info.links[LINK_TIBIA].angle)
     # Check angles
-    if (info.links[LINK_COXA].angle < info.links[LINK_COXA].min_angle or info.links[LINK_COXA].angle > info.links[LINK_COXA].max_angle):
+    if (info.links[LINK_COXA]._angle < info.links[LINK_COXA]._min_angle or info.links[LINK_COXA]._angle > info.links[LINK_COXA]._max_angle):
         return False
-    if (info.links[LINK_FEMUR].angle < info.links[LINK_FEMUR].min_angle or info.links[LINK_FEMUR].angle > info.links[LINK_FEMUR].max_angle):
+    if (info.links[LINK_FEMUR]._angle < info.links[LINK_FEMUR]._min_angle or info.links[LINK_FEMUR]._angle > info.links[LINK_FEMUR]._max_angle):
         return False
     if (info.links[LINK_TIBIA].angle < info.links[LINK_TIBIA].min_angle or info.links[LINK_TIBIA].angle > info.links[LINK_TIBIA].max_angle):
         return False
@@ -104,102 +105,111 @@ def kca(info):
 
 
 
-planes[0].links[LINK_COXA].length = 46.7
-planes[0].links[LINK_COXA].zero_rotate = -35.8
-planes[0].links[LINK_COXA].min_angle = -90
-planes[0].links[LINK_COXA].max_angle = 90
+planes[0]._links[LINK_COXA]._length = 46.7
+planes[0]._links[LINK_COXA]._zero_rotate = -35.8
+planes[0]._links[LINK_COXA]._min_angle = -90
+planes[0]._links[LINK_COXA]._max_angle = 90
 
-planes[0].links[LINK_FEMUR].length = 90
-planes[0].links[LINK_FEMUR].zero_rotate = 90
-planes[0].links[LINK_FEMUR].min_angle = 0
-planes[0].links[LINK_FEMUR].max_angle = 180
+planes[0]._links[LINK_FEMUR]._length = 90
+planes[0]._links[LINK_FEMUR]._zero_rotate = 90
+planes[0]._links[LINK_FEMUR]._min_angle = 0
+planes[0]._links[LINK_FEMUR]._max_angle = 180
 
-planes[0].links[LINK_TIBIA].length = 122.558
-planes[0].links[LINK_TIBIA].zero_rotate = 16.7
-planes[0].links[LINK_TIBIA].min_angle = 0
-planes[0].links[LINK_TIBIA].max_angle = 180
-
-
-planes[1].links[LINK_COXA].length = 46.7
-planes[1].links[LINK_COXA].zero_rotate = 0
-planes[1].links[LINK_COXA].min_angle = -50
-planes[1].links[LINK_COXA].max_angle = 50
-
-planes[1].links[LINK_FEMUR].length = 90
-planes[1].links[LINK_FEMUR].zero_rotate = 90
-planes[1].links[LINK_FEMUR].min_angle = 0
-planes[1].links[LINK_FEMUR].max_angle = 180
-
-planes[1].links[LINK_TIBIA].length = 122.558
-planes[1].links[LINK_TIBIA].zero_rotate = 16.7
-planes[1].links[LINK_TIBIA].min_angle = 0
-planes[1].links[LINK_TIBIA].max_angle = 180
+planes[0]._links[LINK_TIBIA]._length = 122.558
+planes[0]._links[LINK_TIBIA]._zero_rotate = 16.7
+planes[0]._links[LINK_TIBIA]._min_angle = 0
+planes[0]._links[LINK_TIBIA]._max_angle = 180
 
 
+planes[1]._links[LINK_COXA]._length = 46.7
+planes[1]._links[LINK_COXA]._zero_rotate = 0
+planes[1]._links[LINK_COXA]._min_angle = -50
+planes[1]._links[LINK_COXA]._max_angle = 50
 
-planes[2].links[LINK_COXA].length = 46.7
-planes[2].links[LINK_COXA].zero_rotate = 35.8
-planes[2].links[LINK_COXA].min_angle = 90
-planes[2].links[LINK_COXA].max_angle = -90
+planes[1]._links[LINK_FEMUR]._length = 90
+planes[1]._links[LINK_FEMUR]._zero_rotate = 90
+planes[1]._links[LINK_FEMUR]._min_angle = 0
+planes[1]._links[LINK_FEMUR]._max_angle = 180
 
-planes[2].links[LINK_FEMUR].length = 90
-planes[2].links[LINK_FEMUR].zero_rotate = 90
-planes[2].links[LINK_FEMUR].min_angle = 0
-planes[2].links[LINK_FEMUR].max_angle = 180
-
-planes[2].links[LINK_TIBIA].length = 122.558
-planes[2].links[LINK_TIBIA].zero_rotate = 16.7
-planes[2].links[LINK_TIBIA].min_angle = 0
-planes[2].links[LINK_TIBIA].max_angle = 180
+planes[1]._links[LINK_TIBIA]._length = 122.558
+planes[1]._links[LINK_TIBIA]._zero_rotate = 16.7
+planes[1]._links[LINK_TIBIA]._min_angle = 0
+planes[1]._links[LINK_TIBIA]._max_angle = 180
 
 
 
+planes[2]._links[LINK_COXA]._length = 46.7
+planes[2]._links[LINK_COXA]._zero_rotate = 35.8
+planes[2]._links[LINK_COXA]._min_angle = 90
+planes[2]._links[LINK_COXA]._max_angle = -90
 
-planes[3].links[LINK_COXA].length = 46.7
-planes[3].links[LINK_COXA].zero_rotate = 35.8
-planes[3].links[LINK_COXA].min_angle = -90
-planes[3].links[LINK_COXA].max_angle = 90
+planes[2]._links[LINK_FEMUR]._length = 90
+planes[2]._links[LINK_FEMUR]._zero_rotate = 90
+planes[2]._links[LINK_FEMUR]._min_angle = 0
+planes[2]._links[LINK_FEMUR]._max_angle = 180
 
-planes[3].links[LINK_FEMUR].length = 90
-planes[3].links[LINK_FEMUR].zero_rotate = 90
-planes[3].links[LINK_FEMUR].min_angle = 0
-planes[3].links[LINK_FEMUR].max_angle = 180
-
-planes[3].links[LINK_TIBIA].length = 122.558
-planes[3].links[LINK_TIBIA].zero_rotate = 16.7
-planes[3].links[LINK_TIBIA].min_angle = 0
-planes[3].links[LINK_TIBIA].max_angle = 180
-
-
-planes[4].links[LINK_COXA].length = 46.7
-planes[4].links[LINK_COXA].zero_rotate = 0
-planes[4].links[LINK_COXA].min_angle = -50
-planes[4].links[LINK_COXA].max_angle = 50
-
-planes[4].links[LINK_FEMUR].length = 90
-planes[4].links[LINK_FEMUR].zero_rotate = 90
-planes[4].links[LINK_FEMUR].min_angle = 0
-planes[4].links[LINK_FEMUR].max_angle = 180
-
-planes[4].links[LINK_TIBIA].length = 122.558
-planes[4].links[LINK_TIBIA].zero_rotate = 16.7
-planes[4].links[LINK_TIBIA].min_angle = 0
-planes[4].links[LINK_TIBIA].max_angle = 180
+planes[2]._links[LINK_TIBIA]._length = 122.558
+planes[2]._links[LINK_TIBIA]._zero_rotate = 16.7
+planes[2]._links[LINK_TIBIA]._min_angle = 0
+planes[2]._links[LINK_TIBIA]._max_angle = 180
 
 
 
-planes[5].links[LINK_COXA].length = 46.7
-planes[5].links[LINK_COXA].zero_rotate = -35.8
-planes[5].links[LINK_COXA].min_angle = -90
-planes[5].links[LINK_COXA].max_angle = 90
 
-planes[5].links[LINK_FEMUR].length = 90
-planes[5].links[LINK_FEMUR].zero_rotate = 90
-planes[5].links[LINK_FEMUR].min_angle = 0
-planes[5].links[LINK_FEMUR].max_angle = 180
+planes[3]._links[LINK_COXA]._length = 46.7
+planes[3]._links[LINK_COXA]._zero_rotate = 35.8
+planes[3]._links[LINK_COXA]._min_angle = -90
+planes[3]._links[LINK_COXA]._max_angle = 90
 
-planes[5].links[LINK_TIBIA].length = 122.558
-planes[5].links[LINK_TIBIA].zero_rotate = 16.7
-planes[5].links[LINK_TIBIA].min_angle = 0
-planes[5].links[LINK_TIBIA].max_angle = 180
+planes[3]._links[LINK_FEMUR]._length = 90
+planes[3]._links[LINK_FEMUR]._zero_rotate = 90
+planes[3]._links[LINK_FEMUR]._min_angle = 0
+planes[3]._links[LINK_FEMUR]._max_angle = 180
 
+planes[3]._links[LINK_TIBIA]._length = 122.558
+planes[3]._links[LINK_TIBIA]._zero_rotate = 16.7
+planes[3]._links[LINK_TIBIA]._min_angle = 0
+planes[3]._links[LINK_TIBIA]._max_angle = 180
+
+
+planes[4]._links[LINK_COXA]._length = 46.7
+planes[4]._links[LINK_COXA]._zero_rotate = 0
+planes[4]._links[LINK_COXA]._min_angle = -50
+planes[4]._links[LINK_COXA]._max_angle = 50
+
+planes[4]._links[LINK_FEMUR]._length = 90
+planes[4]._links[LINK_FEMUR]._zero_rotate = 90
+planes[4]._links[LINK_FEMUR]._min_angle = 0
+planes[4]._links[LINK_FEMUR]._max_angle = 180
+
+planes[4]._links[LINK_TIBIA]._length = 122.558
+planes[4]._links[LINK_TIBIA]._zero_rotate = 16.7
+planes[4]._links[LINK_TIBIA]._min_angle = 0
+planes[4]._links[LINK_TIBIA]._max_angle = 180
+
+
+
+planes[5]._links[LINK_COXA]._length = 46.7
+planes[5]._links[LINK_COXA]._zero_rotate = -35.8
+planes[5]._links[LINK_COXA]._min_angle = -90
+planes[5]._links[LINK_COXA]._max_angle = 90
+
+planes[5]._links[LINK_FEMUR]._length = 90
+planes[5]._links[LINK_FEMUR]._zero_rotate = 90
+planes[5]._links[LINK_FEMUR]._min_angle = 0
+planes[5]._links[LINK_FEMUR]._max_angle = 180
+
+planes[5]._links[LINK_TIBIA]._length = 122.558
+planes[5]._links[LINK_TIBIA]._zero_rotate = 16.7
+planes[5]._links[LINK_TIBIA]._min_angle = 0
+planes[5]._links[LINK_TIBIA]._max_angle = 180
+
+
+class Point:
+    def __init__(self):
+        self._x = 0
+        self._y = 0
+
+class pt2:
+    def __init__(self):
+        self._pt = [Point(),Point(),Point()]
